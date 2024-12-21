f32f52e33089b94b7ca474645c931f3507519b74
Pavly <pavlysamuelatef@gmail.com>
2024-12-21 19:13:26 +0200

    Format

--- Data Mining Project/test_frequent_pattern.py
+++ Data Mining Project/test_frequent_pattern.py
@@ -1,6 +1,7 @@
 import unittest
 from frequent_pattern import FPTree, FPTreeNode, FrequentPatternManager
 
+
 class TestFPTree(unittest.TestCase):
     def test_fp_tree_node_increment(self):
         node = FPTreeNode("A")
@@ -37,6 +38,7 @@ class TestFPTree(unittest.TestCase):
 
         self.assertEqual(tree.header_table["A"][0].count, 3)
 
+
 class TestFrequentPatternManager(unittest.TestCase):
     def test_build_fp_tree(self):
         transactions = [["A", "B"], ["A", "C"], ["A", "B", "D"]]
@@ -50,7 +52,9 @@ class TestFrequentPatternManager(unittest.TestCase):
     def test_mine_fp_tree(self):
         transactions = [["A", "B"], ["A", "C"], ["A", "B", "D"]]
         fp_tree = FrequentPatternManager.build_fp_tree(transactions)
-        frequent_itemsets = FrequentPatternManager.mine_fp_tree(fp_tree.header_table, min_support=2)
+        frequent_itemsets = FrequentPatternManager.mine_fp_tree(
+            fp_tree.header_table, min_support=2
+        )
 
         expected_itemsets = [
             (["A"], 3),
@@ -66,12 +70,11 @@ class TestFrequentPatternManager(unittest.TestCase):
             (["A", "A"], 3),
             (["B", "C", "C"], 1),
         ]
-        filtered = FrequentPatternManager.filter_items_with_duplicates(frequent_itemsets)
+        filtered = FrequentPatternManager.filter_items_with_duplicates(
+            frequent_itemsets
+        )
 
         expected_filtered = [
             (["A", "B"], 2),
         ]
         self.assertEqual(filtered, expected_filtered)
-
-if __name__ == "__main__":
-    unittest.main()
